cfb95894bf274c5da4cb89a14ddd1ff8c56b7d15
Brienyll <brienyll29@gmail.com>
2017-01-30 13:13:56 -0800

    Update app.jsx

--- app/app.jsx
+++ app/app.jsx
@@ -1,18 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var objOne = {
-  name: 'Brix',
-  location: 'California'
-};
-
-var objTwo = {
-  age: 34,
-  ...objOne
-};
-
-console.log(objTwo);
-
 ReactDOM.render(
   <h1>Boilerplate app!</h1>,
   document.getElementById('app')
