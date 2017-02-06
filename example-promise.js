66793d8be009bd61bb74f22cdee8356b5c9c0c21
Brix <Brix.angeles@gmail.com>
2017-02-06 14:40:56 -0800

    addded es6 promises

--- example-promise.js
+++ example-promise.js
@@ -10,3 +10,16 @@ getTempCallback('Los Angeles', function(err, temp){
     console.log('success', temp)
   }
 });
+
+function getTempPromise (location){
+  return new Promise(function(resolve, reject){
+    resolve(79);
+    reject('City not found');
+  });
+}
+
+getTempPromise('Los Angeles').then(function(temp){
+  console.log('promis success', temp);
+}, function(err){
+  consolel.log('promise error', err);
+})
