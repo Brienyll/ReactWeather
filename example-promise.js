dc2ad868443e6f6244e771f023f26de300017f74
Brix <Brix.angeles@gmail.com>
2017-02-09 13:30:41 -0800

    added challenge for promise

--- example-promise.js
+++ example-promise.js
@@ -1,27 +1,50 @@
-function getTempCallback (location, callback){
-  callback(undefined, 78);
-  callback('City not found');
-}
+// function getTempCallback (location, callback){
+//   callback(undefined, 78);
+//   callback('City not found');
+// }
+//
+// getTempCallback('Los Angeles', function(err, temp){
+//   if (err){
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp)
+//   }
+// });
+//
+// function getTempPromise (location){
+//   return new Promise(function(resolve, reject){
+//     setTimeout(function(){
+//       resolve(79);
+//       reject('City not found');
+//     }, 1000);
+//   });
+// }
+//
+// getTempPromise('Los Angeles').then(function(temp){
+//   console.log('promis success', temp);
+// }, function(err){
+//   consolel.log('promise error', err);
+// })
 
-getTempCallback('Los Angeles', function(err, temp){
-  if (err){
-    console.log('error', err);
-  } else {
-    console.log('success', temp)
-  }
-});
-
-function getTempPromise (location){
-  return new Promise(function(resolve, reject){
-    setTimeout(function(){
-      resolve(79);
-      reject('City not found');
-    }, 1000);
+//Challenge Area
+function addPromise(a, b){
+  return new Promise (function(resolve, reject) {
+    if (typeof a === 'number' && typeof b === 'number'){
+      resolve(a + b);
+    } else {
+      reject('A & b need to be numbers');
+    }
   });
 }
 
-getTempPromise('Los Angeles').then(function(temp){
-  console.log('promis success', temp);
-}, function(err){
-  consolel.log('promise error', err);
-})
+addPromise(2, 3).then(function(sum) {
+  console.log('success', sum);
+}, function (err) {
+  console.log('error', err);
+});
+
+addPromise('andrew', 9).then(function(sum){
+  console.log('this should not show up');
+}, function (err) {
+  console.log('this should appear', err)
+});
